e766b4173527b726d1f67b83b795debfaaa12607
erban <943186182@qq.com>
2021-01-13 23:39:46 +0800

    第68题完成

--- algorithm_0067.cpp
+++ algorithm_0067.cpp
@@ -24,6 +24,7 @@
  */
 
 #include <string>
+#include <iostream>
 using namespace std;
 
 
@@ -31,14 +32,103 @@ using namespace std;
 class Solution {
 public:
     string addBinary(string a, string b) {
-        std::string* tmp = a.size() >=b.size() ? &a : &b;
+        std::string* tmp_long = nullptr;
+        std::string* tmp_short = nullptr;
+        int short_len = 0;
+        int long_len = 0;
         char tmp2 = 0;
+        if(a.size() >= b.size())
+        {
+            tmp_long = &a;
+            long_len = a.size();
+            tmp_short = &b;
+            short_len = b.size();
+        }
+        else
+        {
+            tmp_long = &b;
+            long_len = b.size();
+            tmp_short = &a;
+            short_len = a.size();
+        }
+        for(int i = short_len-1,j=long_len-1;i>=0;--i,--j)
+        {
+            if(tmp_long->at(j)=='0' && tmp_short->at(i)=='0' && tmp2 == 0)
+            {
+            }
+            else if(tmp_long->at(j) == '0' && tmp_short->at(i) == '0' && tmp2 ==1)
+            {
+                tmp_long->at(j) = '1';
+                tmp2 = 0;
+            }
+            else if(tmp_long->at(j) == '0' && tmp_short->at(i) == '1' && tmp2 == 0)
+            {
+                tmp_long->at(j) = '1';
+            }
+            else if(tmp_long->at(j) == '0' && tmp_short->at(i) == '1' && tmp2 == 1)
+            {
+            }
+            else if (tmp_long->at(j) == '1' && tmp_short->at(i) == '0' && tmp2 == 0)
+            {
 
+            }
+            else if(tmp_long->at(j) == '1' && tmp_short->at(i) == '0' && tmp2 == 1)
+            {
+                tmp_long->at(j) = '0';
+            }
+            else if (tmp_long->at(j) == '1' && tmp_short->at(i) == '1' && tmp2 == 0)
+            {
+                tmp_long->at(j) = '0';
+                tmp2 = 1;
+            }
+            else if(tmp_long->at(j) == '1' && tmp_short->at(i)== '1' && tmp2 == 1)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+        for(int i = long_len - short_len-1;i>=0;--i)
+        {
+            if(tmp_long->at(i) == '0' && tmp2 ==0)
+            {
+                break;
+            }
+            else if(tmp_long->at(i) == '0' && tmp2 == 1)
+            {
+                tmp_long->at(i) = '1';
+                tmp2 = 0;
+                break;
+            }
+            else if (tmp_long->at(i) == '1' && tmp2 == 0)
+            {
+                break;
+            }
+            else if(tmp_long->at(i) == '1' && tmp2 == 1)
+            {
+                tmp_long->at(i) = '0';
+                tmp2 = 1;
+            }
+            else
+            {
+
+            }
+        }
+        if(tmp2 == 1)
+        {
+            tmp_long->insert(tmp_long->begin(),'1');
+        }
+        return *tmp_long;
     }
+
 };
 
 
 int main(int argc,char* argv[])
 {
+    Solution so;
+    std::cout << so.addBinary("11","1")<<std::endl;
     return 0;
 }
