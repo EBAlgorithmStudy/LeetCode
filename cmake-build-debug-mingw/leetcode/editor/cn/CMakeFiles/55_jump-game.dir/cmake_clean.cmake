e98165b1cd4140f80a3e8ef33681532e28d9bd10
erban <943186182@qq.com>
2021-06-18 11:26:12 +0800

    由于一些原因，原先的提交记录丢失，重新提交

---
+++ cmake-build-debug-mingw/leetcode/editor/cn/CMakeFiles/55_jump-game.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "55_jump-game.exe"
+  "55_jump-game.exe.manifest"
+  "55_jump-game.pdb"
+  "CMakeFiles/55_jump-game.dir/55_jump-game.cpp.obj"
+  "lib55_jump-game.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/55_jump-game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
