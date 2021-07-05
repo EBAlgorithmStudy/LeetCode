b0bede7539c2963767d7eb3b48c6b85a283b1a8c
erban <943186182@qq.com>
2021-07-05 14:11:12 +0800

    日常提交

---
+++ leetcode/editor/cn/41_first-missing-positive.cpp
@@ -0,0 +1,54 @@
+//给你一个未排序的整数数组 nums ，请你找出其中没有出现的最小的正整数。 
+//请你实现时间复杂度为 O(n) 并且只使用常数级别额外空间的解决方案。
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [1,2,0]
+//输出：3
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [3,4,-1,1]
+//输出：2
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [7,8,9,11,12]
+//输出：1
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 5 * 105 
+// -231 <= nums[i] <= 231 - 1 
+// 
+// Related Topics 数组 哈希表 
+// 👍 1117 👎 0
+
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main(int argc,char* argv[])
+{
+    return 0;
+}
